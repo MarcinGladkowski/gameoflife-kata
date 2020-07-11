1888cd30effdd50fede09587e0d45dd260e990a6
Martin89PL <marcingladkowski@gmail.com>
2020-07-11 12:09:50 +0200

    test: unit test for neighbors for cell

--- kata-boilerplate/test/Game.test.js
+++ kata-boilerplate/test/Game.test.js
@@ -5,14 +5,31 @@ const AreaParser = require('../src/AreaParser.js')
 
 describe("Game", () => {
     
-    const parsedArea = [ [ '.', '.', '.', '.', '.', '.', '.', '.' ],
-    [ '.', '.', '.', '.', '*', '.', '.', '.' ],
-    [ '.', '.', '.', '*', '*', '.', '.', '.' ],
-    [ '.', '.', '.', '.', '.', '.', '.', '.' ] ]
+    const parsedArea = [ 
+        [ '.', '.', '.', '.', '.', '.', '.', '.' ],
+        [ '.', '.', '.', '.', '*', '.', '.', '.' ],
+        [ '.', '.', '.', '*', '*', '.', '.', '.' ],
+        [ '.', '.', '.', '.', '.', '.', '.', '.' ] 
+    ]
 
     test("it should return initial step", () => {
         const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
         const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
         expect(game.steps.length).toBe(1);
+    });
+
+    test("it should return initial step", () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.steps.length).toBe(1);
+    });
+
+    test('test should return neighbours for cell', () => {
+
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+
+        expect(game.getCellNeighbors(0, 0, parsedArea)).toEqual(['.','.','.'])
+
     });
 });
