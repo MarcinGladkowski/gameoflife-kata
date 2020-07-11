16880dec367f73702e2e6c69fd4c01664c3e37c1
Martin89PL <marcingladkowski@gmail.com>
2020-07-11 12:09:19 +0200

    feat: get all neightbors for cell by coors

--- kata-boilerplate/src/Game.js
+++ kata-boilerplate/src/Game.js
@@ -28,6 +28,31 @@ class Game {
         // create next generation
     }
 
+    getCellNeighbors(x, y, area) {
+ 
+        const neighbors = []
+        if (isUndefined(area[x-1])) {
+            if (isUndefined(area[x-1][y-1])) neighbors.push(area[x-1][y-1])
+            if (isUndefined(area[x-1][y])) neighbors.push(area[x-1][y])
+            if (isUndefined(area[x-1][y+1])) neighbors.push(area[x-1][y+1])
+        }
+        
+        if (typeof(area[x][y-1]) != 'undefined') neighbors.push(area[x][y-1])
+        if (typeof(area[x][y+1]) != 'undefined') neighbors.push(area[x][y+1])
+
+        if (isUndefined(area[x+1])) {
+            if (isUndefined(area[x+1][y-1])) neighbors.push(area[x+1][y-1])
+            if (isUndefined(area[x+1][y])) neighbors.push(area[x+1][y])
+            if (isUndefined(area[x+1][y+1])) neighbors.push(area[x+1][y+1])
+        }
+      
+        function isUndefined(value) {
+            return typeof(value) != 'undefined'
+        }
+
+        return neighbors;
+    }
+
     steps() {
         return this.steps;
     }
