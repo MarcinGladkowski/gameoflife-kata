ea300c1b92c287310d61d8f942db5427f061ff27
Martin89PL <marcingladkowski@gmail.com>
2020-07-11 11:03:23 +0200

    feat: exclude methods to split row and elements

--- kata-boilerplate/src/AreaParser.js
+++ kata-boilerplate/src/AreaParser.js
@@ -1,9 +1,16 @@
 class AreaParser {
     parse(area) {
-        const parsedArea = area.replace(/\r/g, "").split(/\n/);
+        const parsedArea = this.splitRows(area);
         parsedArea.shift();
-        const moreParsed = parsedArea.map(row => row.split(''));
-        return moreParsed;
+        return this.splitElements(parsedArea)
+    }
+
+    splitRows(area) {
+        return area.replace(/\r/g, "").split(/\n/);
+    }
+
+    splitElements(area) {
+        return area.map(row => row.split(''));
     }
 }
 
