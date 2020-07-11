a696ac74a4c68cc81b60dc8ee7c644dbdd4ac159
Martin89PL <marcingladkowski@gmail.com>
2020-07-11 12:08:58 +0200

    test: remove unused tests

--- kata-boilerplate/test/AreaValidator.test.js
+++ kata-boilerplate/test/AreaValidator.test.js
@@ -12,14 +12,4 @@ describe("Area validator", () => {
     test("it should return true", () => {
         expect(AreaValidator.valid(input, parsedArea)).toBe(true)
     });
-
-    // test("it should return false - height is invalid", () => {
-    //     const firstStepOfParsing = [ '8 8', '........', '....*...', '...**...', '........' ];
-    //     expect(AreaValidator.valid(firstStepOfParsing)).toBe(false)
-    // });
-
-    // test("it should return false - width is invalid", () => {
-    //     const firstStepOfParsing = [ '8 2', '........', '....*...', '...**...', '........' ];
-    //     expect(AreaValidator.valid(firstStepOfParsing)).toBe(false)
-    // });
 });
