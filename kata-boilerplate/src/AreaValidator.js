98d6e74f658726691497b4c9ea4b60df68e2c744
Martin89PL <marcingladkowski@gmail.com>
2020-07-11 11:23:15 +0200

    test: validator comparasion init area to parsed

--- kata-boilerplate/src/AreaValidator.js
+++ kata-boilerplate/src/AreaValidator.js
@@ -1,11 +1,10 @@
 class AreaValidator {
-    static valid(parsedArea) {
-        const [rows, rowElements] = parsedArea[0].split(/\s/);
-
-        if ((parsedArea.length - 1) != rows) return false // or throw Error(`Invalid number of rows ${parsedArea.length - 1}. Should be: ${rows}`)
+    static valid(initArea, parsedArea) {
+        const [rows, rowElements] = initArea.replace(/\r/g, "").split(/\n/)[0].split(/\s/);
+        
+        if ((parsedArea.length) != rows) return false // or throw Error(`Invalid number of rows ${parsedArea.length - 1}. Should be: ${rows}`)
 
         parsedArea.forEach((row, index) => {
-            if (index == 0) return;
             if (row.length != rowElements) return false; // or throw Error(`Invalid number of elements in row. Should be: ${rowElements}`)
         });
 
