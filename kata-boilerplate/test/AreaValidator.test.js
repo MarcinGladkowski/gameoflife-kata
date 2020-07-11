3f6ca37895979ff2057d895098fed2f0dc02b009
Martin89PL <marcingladkowski@gmail.com>
2020-07-11 11:23:40 +0200

    test: change test to comparasion area

--- kata-boilerplate/test/AreaValidator.test.js
+++ kata-boilerplate/test/AreaValidator.test.js
@@ -1,18 +1,25 @@
+const fs = require('fs')
 const AreaValidator = require('../src/AreaValidator.js')
 
 describe("Area validator", () => {
+
+    const input = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+    const parsedArea = [ [ '.', '.', '.', '.', '.', '.', '.', '.' ],
+    [ '.', '.', '.', '.', '*', '.', '.', '.' ],
+    [ '.', '.', '.', '*', '*', '.', '.', '.' ],
+    [ '.', '.', '.', '.', '.', '.', '.', '.' ] ]
+
     test("it should return true", () => {
-        const firstStepOfParsing = [ '4 8', '........', '....*...', '...**...', '........' ];
-        expect(AreaValidator.valid(firstStepOfParsing)).toBe(true)
+        expect(AreaValidator.valid(input, parsedArea)).toBe(true)
     });
 
-    test("it should return false - height is invalid", () => {
-        const firstStepOfParsing = [ '8 8', '........', '....*...', '...**...', '........' ];
-        expect(AreaValidator.valid(firstStepOfParsing)).toBe(false)
-    });
+    // test("it should return false - height is invalid", () => {
+    //     const firstStepOfParsing = [ '8 8', '........', '....*...', '...**...', '........' ];
+    //     expect(AreaValidator.valid(firstStepOfParsing)).toBe(false)
+    // });
 
-    test("it should return false - width is invalid", () => {
-        const firstStepOfParsing = [ '8 2', '........', '....*...', '...**...', '........' ];
-        expect(AreaValidator.valid(firstStepOfParsing)).toBe(false)
-    });
+    // test("it should return false - width is invalid", () => {
+    //     const firstStepOfParsing = [ '8 2', '........', '....*...', '...**...', '........' ];
+    //     expect(AreaValidator.valid(firstStepOfParsing)).toBe(false)
+    // });
 });
