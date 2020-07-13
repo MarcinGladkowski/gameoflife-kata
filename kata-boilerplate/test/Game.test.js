30f68286b2a5ea309ab575e9e6f808451973a1ba
Martin89PL <marcingladkowski@gmail.com>
2020-07-13 08:13:01 +0200

    test: add test for rules of live/dead cell

--- kata-boilerplate/test/Game.test.js
+++ kata-boilerplate/test/Game.test.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const Game = require('../src/Game.js')
 const AreaValidator = require('../src/AreaValidator.js')
 const AreaParser = require('../src/AreaParser.js')
+const CellType = require('../src/CellType')
 
 describe("Game", () => {
     
@@ -24,12 +25,58 @@ describe("Game", () => {
         expect(game.steps.length).toBe(1);
     });
 
-    test('test should return neighbours for cell', () => {
-
+    test('test should return neighbors for cell', () => {
         const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
         const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
-
         expect(game.getCellNeighbors(0, 0, parsedArea)).toEqual(['.','.','.'])
+    });
 
+
+    test('test should return dead neighbors cell', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.getDeadNeighbors(['.','.','.'])).toEqual(3);
+    });
+
+    test('test should return live neighbors cell', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.getLiveNeighbors(['.','*','*'])).toEqual(2);
+    });
+
+
+    test('test should dead by overcrowd', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.decide([CellType.LIVE, CellType.LIVE, CellType.LIVE, CellType.LIVE], CellType.LIVE)).toEqual(CellType.DEAD);
+    });
+
+    test('test should dead by underpopulation', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.decide([CellType.LIVE], CellType.LIVE)).toEqual(CellType.DEAD);
+    });
+
+    test('test should live to next generation', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.decide([CellType.LIVE, CellType.LIVE], CellType.LIVE)).toEqual(CellType.LIVE);
+        expect(game.decide([CellType.LIVE, CellType.LIVE, CellType.LIVE], CellType.LIVE)).toEqual(CellType.LIVE);
+    });
+
+    test('test should become cell live from dead', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        expect(game.decide([CellType.LIVE, CellType.LIVE, CellType.LIVE], CellType.DEAD)).toEqual(CellType.LIVE);
+    });
+
+    test('test should create next generation', () => {
+        const initialArea = fs.readFileSync(__dirname + '/examples/example01-input.txt', 'utf8')
+        const game = new Game(new AreaParser(), new AreaValidator(), initialArea);
+        const newGeneration = game.nextGeneration()
+        const consoleOutputFormat = game.consoleOutputFormat(newGeneration);
+        const areaNextGenerationOutput = fs.readFileSync(__dirname + '/examples/example01-output.txt', 'utf8')
+
+        expect(consoleOutputFormat).toBe(areaNextGenerationOutput);
     });
 });
